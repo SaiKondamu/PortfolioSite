b0d724221923e498dc54aadea25c40271e3aa490
Sai Kondamu <saikondamu@gmail.com>
2024-08-09 18:40:04 -0400

    adding skills section

---
+++ force-app/main/default/lwc/skills/skills.js
@@ -0,0 +1,71 @@
+import { LightningElement } from 'lwc';
+
+export default class Skills extends LightningElement {
+
+    skills = [
+        {
+            name: 'Java',
+            level: 'Expert'
+        },
+        {
+            name: 'Apex',
+            level: 'Intermediate'
+        }, 
+        {
+            name: 'JavaScript',
+            level: 'Intermediate'
+        },
+        {
+            name: 'HTML',
+            level: 'Intermediate'
+        },
+        {
+            name: 'CSS',
+            level: 'Intermediate'
+        },
+        {
+            name: 'SQL',
+            level: 'Intermediate'
+        },
+        {
+            name: 'Git',
+            level: 'Intermediate'
+        },
+        {
+            name: 'Salesforce',
+            level: 'Expert'
+        },
+        {
+            name: 'React',
+            level: 'Intermediate'
+        },
+        {
+            name: 'Salesforce Platform',
+            level: 'Expert'
+        },
+        {
+            name: 'Salesforce Lightning',
+            level: 'Expert'
+        },
+        {
+            name: 'Asynchronous Apex',
+            level: 'Intermediate'
+        },
+        {
+            name: 'Salesforce Admin Skills',
+            level: 'Expert'
+        },
+        {
+            name: 'Aura Components',
+            level: 'Intermediate'
+        },
+        {
+            name: 'Lightning Web Components',
+            level: 'Expert'
+        },
+        {
+            name: 'Python',
+            level: 'Intermediate'
+        }
+    ]
+}
